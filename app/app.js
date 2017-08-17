43e3e9c70da79476083b6ece7008d2bb239f77a3
Michael Lynch <michael@ketohub.io>
2017-08-16 23:06:02 -0400

    Moving thumbnail logic to app.js

--- app/app.js
+++ app/app.js
@@ -6,4 +6,15 @@ ketoHubApp.controller('ketoHubCtrl',
                       function($scope, $firebaseObject, $window) {
   var ref = $window.firebase.database().ref();
   $scope.recipes = $firebaseObject(ref.child('recipes'));
+
+  $scope.recipes.$loaded()
+    .then(function() {
+      // Add thumbnail attribute to each recipe.
+      for (var key in $scope.recipes) {
+        if (key.lastIndexOf('$', 0) !== 0 && key != 'forEach') {
+          $scope.recipes[key].thumbnail_url =
+            'https://storage.googleapis.com/ketohub/' + key + '_thumbnail.jpg';
+        }
+      }
+    });
 });
