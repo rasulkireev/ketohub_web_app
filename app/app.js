13d32596ad06e1b76a5515e859ca20356c88082e
Michael Lynch <michael@ketohub.io>
2017-08-19 13:49:48 -0400

    temp commit

--- app/app.js
+++ app/app.js
@@ -2,13 +2,24 @@
 
 var ketoHubApp = angular.module('ketoHubApp', ['firebase']);
 
-ketoHubApp.controller('ketoHubCtrl',
-                      function($scope, $firebaseObject, $window) {
+ketoHubApp
+.constant('buttonActiveClass', 'btn-primary')
+.constant('recipesPerPage', 2)
+.controller('ketoHubCtrl',
+            function($scope, $firebaseArray, $window, buttonActiveClass, recipesPerPage) {
+  $scope.currentPage = 1;
+  $scope.pageSize = recipesPerPage;
+
   var ref = $window.firebase.database().ref();
-  $scope.recipes = $firebaseObject(ref.child('recipes'));
+  $scope.recipes = $firebaseArray(ref.child('recipes'));
+  console.log('got recipes');
+  console.log($scope.recipes);
 
   $scope.recipes.$loaded()
     .then(function() {
+  		console.log('loaded recipes');
+		  console.log($scope.recipes);
+
       // Add thumbnail attribute to each recipe.
       for (var key in $scope.recipes) {
         if (key.lastIndexOf('$', 0) !== 0 && key != 'forEach') {
@@ -17,9 +28,18 @@ ketoHubApp.controller('ketoHubCtrl',
         }
       }
     });
+
+  $scope.selectPage = function(newPage) {
+    $scope.currentPage = newPage;
+  };
+
+  $scope.getPageClass = function(page) {
+    return $scope.currentPage == page ? buttonActiveClass : '';
+  };
 });
 
-ketoHubApp.filter('rootDomain', function() {
+ketoHubApp
+.filter('rootDomain', function() {
   var extractHostname = function(url) {
     var hostname;
 
@@ -50,4 +70,35 @@ ketoHubApp.filter('rootDomain', function() {
     }
     return domain;
   };
+})
+.filter('range', function($filter) {
+  return function (data, page, size) {
+    console.log('in range filter');
+    console.log(data);
+    console.log(page);
+    console.log(size);
+    if (angular.isArray(data) && angular.isNumber(page) && angular.isNumber(size)) {
+      var startIndex = (page - 1) * size;
+      if (data.length < startIndex) {
+        return [];
+      } else {
+        return $filter('limitTo')(data.splice(startIndex), size);
+      }
+    } else {
+      return data;
+    }
+  }
+})
+.filter('pageCount', function() {
+  return function(data, size) {
+    if (angular.isArray(data)) {
+      var result = [];
+      for (var i = 0; i < Math.ceil(data.length / size); i++) {
+        result.push(i);
+      }
+      return result;
+    } else {
+      return data;
+    }
+  }
 });
