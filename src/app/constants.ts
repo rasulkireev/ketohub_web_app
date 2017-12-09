77f4ca4e80f765ab6692b5391503b4a7ca3163d3
Michael Lynch <mtlynch@users.noreply.github.com>
2017-12-08 21:59:25 -0500

    Adjusting look of category buttons (#125)

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,5 +1,5 @@
 const recipesPerPage = 15;
 const maxPageButtons = 6;
-const recipeCategories = ['breakfast', 'entree', 'side', 'dessert', 'snack', 'beverage', 'condiment'];
+const recipeCategories = ['entree', 'breakfast', 'side', 'dessert', 'snack', 'beverage', 'condiment'];
 
 export { recipesPerPage, maxPageButtons, recipeCategories };
