811fec1872f90941bb28563cac6c4aa93667208a
Michael Lynch <mtlynch@users.noreply.github.com>
2017-12-12 18:39:51 -0500

    Switching all CSS files to SASS (#136)

--- src/app/_components/recipe-list/recipe-list.component.ts
+++ src/app/_components/recipe-list/recipe-list.component.ts
@@ -9,7 +9,7 @@ import { recipesPerPage, maxPageButtons, recipeCategories } from '../../constant
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css'],
+  styleUrls: ['./recipe-list.component.scss'],
 })
 export class RecipeListComponent implements OnInit {
 
