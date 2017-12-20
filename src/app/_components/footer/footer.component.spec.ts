8ff42639d5cd1fa5f38c035eb123606162f97c44
Michael Lynch <mtlynch@users.noreply.github.com>
2017-12-20 00:46:31 -0500

    Overhauling UI (#155)
    * Overhauling UI
    
    Integrating new logo
    Changing fonts
    Making everything more responsive
    
    * Simplifying nav CSS

--- src/app/_components/footer/footer.component.spec.ts
+++ src/app/_components/footer/footer.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { FooterComponent } from './footer.component';
+import { MailingListComponent } from '../mailing-list/mailing-list.component';
 
 describe('FooterComponent', () => {
   let component: FooterComponent;
@@ -8,7 +9,7 @@ describe('FooterComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [FooterComponent],
+      declarations: [FooterComponent, MailingListComponent],
     })
     .compileComponents();
   }));
