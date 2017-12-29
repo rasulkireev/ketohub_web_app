8b9d0bee41565364b98830b6941c21ab28cec358
Michael Lynch <michael@ketohub.io>
2017-12-29 16:12:56 -0500

    Adding protractor docker setup (non-working)

--- protractor.conf.js
+++ protractor.conf.js
@@ -14,6 +14,7 @@ exports.config = {
   directConnect: true,
   baseUrl: 'http://localhost:4200/',
   framework: 'jasmine',
+  seleniumAddress: 'http://localhost:4444/wd/hub',
   jasmineNodeOpts: {
     showColors: true,
     defaultTimeoutInterval: 30000,
