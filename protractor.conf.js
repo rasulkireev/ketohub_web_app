62eada546d624b47b2f8416cdd4b74ed274bccdc
Anastasiia Prokhorova <prokhorova.ann26@gmail.com>
2018-01-09 08:02:10 +0400

    add some e2e tests - fix protractor conf

--- protractor.conf.js
+++ protractor.conf.js
@@ -1,6 +1,3 @@
-// Protractor configuration file, see link for more information
-// https://github.com/angular/protractor/blob/master/lib/config.ts
-
 const { SpecReporter } = require('jasmine-spec-reporter');
 
 exports.config = {
@@ -10,14 +7,13 @@ exports.config = {
   ],
   capabilities: {
     'browserName': 'chrome',
-    'chromeOptions': {
-      args: ['--no-sandbox']
+    chromeOptions: {
+      args: [ '--headless', '--no-sandbox', '--disable-gpu', '--window-size=800,600' ]
     }
   },
   directConnect: true,
   baseUrl: 'http://localhost:4200/',
   framework: 'jasmine',
-  seleniumAddress: 'http://localhost:4444/wd/hub',
   jasmineNodeOpts: {
     showColors: true,
     defaultTimeoutInterval: 30000,
