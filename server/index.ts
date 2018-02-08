b32875b1292a8943b52fc7dc962dfac6b5ef34ec
prohorova <prokhorova.ann26@gmail.com>
2018-02-08 23:14:15 +0400

    delete console.log

--- server/index.ts
+++ server/index.ts
@@ -1,7 +1,5 @@
 import * as angularUniversal from 'angular-universal-express-firebase';
 
-console.log(__dirname + '/index.html');
-
 export let ssrapp = angularUniversal.trigger({
   index: __dirname + '/index.html',
   main: __dirname + '/dist-server/main.bundle',
