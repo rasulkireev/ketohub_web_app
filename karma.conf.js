e1452f50b9e7bf4f6c080e303d8c966553e68853
prohorova <prokhorova.ann26@gmail.com>
2018-04-27 02:30:03 +0400

    karma conf

--- karma.conf.js
+++ karma.conf.js
@@ -1,20 +1,38 @@
 module.exports = function (config) {
+  var webdriverConfig = {
+    hostname: 'selenium',
+    port: 4444,
+  };
+
   config.set({
     basePath: '',
     frameworks: ['jasmine', '@angular/cli'],
     plugins: [
       require('karma-jasmine'),
-      require('karma-chrome-launcher'),
+      require('karma-selenium-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
       require('@angular/cli/plugins/karma')
     ],
     customLaunchers: {
-      ChromeHeadlessCI: {
-        base: 'ChromeHeadless',
-        flags: ['--no-sandbox']
-      }
-    },
+      'firefox': {
+        base: 'WebDriver',
+        config: webdriverConfig,
+        browserName: 'firefox',
+        name: 'ff',
+        platform: 'WIN8_1',
+        platformName: 'windows',
+        version: '54',
+      },
+      'chrome': {
+        base: 'WebDriver',
+        config: webdriverConfig,
+        browserName: 'chrome',
+        name: 'Chrome',
+        platform: 'VISTA',
+        platformName: 'windows',
+        version: 'latest',
+      },
     client:{
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
@@ -37,12 +55,11 @@ module.exports = function (config) {
     reporters: config.angularCli && config.angularCli.codeCoverage
               ? ['progress', 'coverage-istanbul']
               : ['progress', 'kjhtml'],
-    hostname: 'selenium',
     port: 4444,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['ChromeHeadless'],
+    browsers: ['firefox','chrome'],
     singleRun: false,
     browserNoActivityTimeout: 60000
   });
