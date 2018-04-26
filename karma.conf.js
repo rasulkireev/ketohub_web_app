c71a15a57d9041db67d8eae807cf48f1ed7f8f87
prohorova <prokhorova.ann26@gmail.com>
2018-04-27 03:24:11 +0400

    karma conf

--- karma.conf.js
+++ karma.conf.js
@@ -9,7 +9,6 @@ module.exports = function (config) {
     frameworks: ['jasmine', '@angular/cli'],
     plugins: [
       require('karma-jasmine'),
-      require('karma-selenium-launcher'),
       require('karma-chrome-launcher'),
       require('karma-firefox-launcher'),
       require('karma-jasmine-html-reporter'),
@@ -57,7 +56,7 @@ module.exports = function (config) {
     reporters: config.angularCli && config.angularCli.codeCoverage
               ? ['progress', 'coverage-istanbul']
               : ['progress', 'kjhtml'],
-    port: 4444,
+    port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
