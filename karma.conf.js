c42dd8a0a39fe22a739d2b527c0070f05ac8fdbb
prohorova <prokhorova.ann26@gmail.com>
2018-04-27 16:49:19 +0400

    karma conf

--- karma.conf.js
+++ karma.conf.js
@@ -1,11 +1,15 @@
 module.exports = function (config) {
+  var webdriverConfig = {
+    hostname: 'localhost',
+    port: 4444,
+    path: '/wd/hub'
+  };
+
   config.set({
     basePath: '',
     frameworks: ['jasmine', '@angular/cli'],
     plugins: [
       require('karma-jasmine'),
-      require('karma-chrome-launcher'),
-      require('karma-firefox-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
       require('@angular/cli/plugins/karma')
@@ -32,15 +36,19 @@ module.exports = function (config) {
     reporters: config.angularCli && config.angularCli.codeCoverage
               ? ['progress', 'coverage-istanbul']
               : ['progress', 'kjhtml'],
-    protocol: 'http:',
-    hostname: 'selenium',
-    //listenAddress: '127.0.0.1',
-    port: 4444,
-    urlRoot: '/wd/hub',
+    port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Firefox','Chrome'],
+    customLaunchers: {
+      selenium_chrome: {
+        base: 'Selenium',
+        config: webdriverConfig,
+        name: 'Karma Test',
+        browserName: 'chrome'
+      }
+    },
+    browsers: ['selenium_chrome'],
     singleRun: false,
     browserNoActivityTimeout: 60000
   });
