4ff653c1520c4a52b9557bab2197e19e14038830
Michael Lynch <michael@ketohub.io>
2019-01-06 00:09:59 -0500

    Fixing bug in array-sort pipe

--- src/app/_pipes/array-sort/array-sort.pipe.spec.ts
+++ src/app/_pipes/array-sort/array-sort.pipe.spec.ts
@@ -1,8 +1,41 @@
 import { ArraySortPipe } from './array-sort.pipe';
 
 describe('ArraySortPipe', () => {
+  const pipe = new ArraySortPipe();
+
   it('create an instance', () => {
-    const pipe = new ArraySortPipe();
     expect(pipe).toBeTruthy();
+  });
+
+  it('should sort objects in descending order', () => {
+    expect(pipe.transform([
+      { letter: 'c' },
+      { letter: 'a' },
+      { letter: 'b' },
+    ], 'letter')).toEqual([
+      { letter: 'c' },
+      { letter: 'b' },
+      { letter: 'a' },
+    ]);
+  });
+
+  it('should sort undefined fields at the end', () => {
+    expect(pipe.transform([
+      { letter: 'c' },
+      { letter: 'm' },
+      { letter: 'r' },
+      { letter: 'z' },
+      { letter: 'y' },
+      { banana: 'yes' },
+      { letter: 'b' },
+    ], 'letter')).toEqual([
+      { letter: 'z' },
+      { letter: 'y' },
+      { letter: 'r' },
+      { letter: 'm' },
+      { letter: 'c' },
+      { letter: 'b' },
+      { banana: 'yes' },
+    ]);
   });
 });
