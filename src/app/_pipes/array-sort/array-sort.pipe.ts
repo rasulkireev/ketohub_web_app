52787aabb7acd98b9e515e6ec5d7825e54c0d6a7
Michael Lynch <mtlynch@users.noreply.github.com>
2019-01-05 21:16:42 -0800

    Fixing bug in array-sort pipe (#252)

--- src/app/_pipes/array-sort/array-sort.pipe.ts
+++ src/app/_pipes/array-sort/array-sort.pipe.ts
@@ -7,6 +7,15 @@ export class ArraySortPipe implements PipeTransform {
 
   transform(array: any[], field: string): any[] {
     array.sort((a: any, b: any) => {
+      if (a[field] && !b[field]) {
+        return -1;
+      }
+      if (b[field] && !a[field]) {
+        return 1;
+      }
+      if (!a[field] && !b[field]) {
+        return 0;
+      }
       if (a[field] < b[field]) {
         return 1;
       }
